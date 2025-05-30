345d6b358d587a141d16ffa34ef70fdad8ec2304
Baipalm <ttamajai@ucsb.edu>
2025-05-29 21:25:43 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -18,8 +18,8 @@ df = pd.DataFrame({
         'y': oranges
     })
 chart = alt.Chart(df).mark_line().encode(
-    alt.X('x:Q', scale=alt.Scale(domain=[0,e_x * np.sqrt(L)], nice=False)),
-    alt.Y('y:Q', scale=alt.Scale(domain=[0,e_y * np.sqrt(L)])),
+    alt.X('x:Q', scale=alt.Scale(domain=[0,e_x * np.sqrt(L)*1.1], nice=False)),
+    alt.Y('y:Q', scale=alt.Scale(domain=[0,e_y * np.sqrt(L)*1.1])),
 )
 st.altair_chart(chart.properties(width=600, height=600)) 
 
