069a69524956f656be6489cdcf7cacfe65d838fb
Baipalm <ttamajai@ucsb.edu>
2025-05-29 20:12:09 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -5,13 +5,15 @@ import numpy as np
 st.title("Production Possibility Curves")
 st.sidebar.header("Settings")
 L = st.sidebar.slider("Total Labour", 10, 100, 50, step=10)
-e_x = st.sidebar.slider("efficiency of oranges", 1, 10, 5, step=1)
-e_y = st.sidebar.slider("efficiency of frogs", 1, 10, 5, step=1)
+e_x = st.sidebar.slider("efficiency of frogs", 1, 10, 5, step=1)
+e_y = st.sidebar.slider("efficiency of oranges", 1, 10, 5, step=1)
 
-x = np.arange(0,200,1)
-y = e_y*np.sqrt(L-(x/e_x)**2)
+frogs = np.arange(0,200,1)
+oranges = e_y*np.sqrt(L-(x/e_x)**2)
 
 fig = plotly.line(x=x,y=y)
+fig.layout.xaxis.fixedrange = True
+fig.layout.yaxis.fixedrange = True
 fig.update_layout(
        xaxis_range=[0, 30],  # Set x-axis range from 0 to 4
        yaxis_range=[0, 30],   # Set y-axis range from 0 to 7
