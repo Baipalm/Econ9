2ce60d54f0bb19c6b3ed9e2c6a5d2a08e179ae26
Baipalm <ttamajai@ucsb.edu>
2025-05-29 20:23:41 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as plotly
 import numpy as np
+import pandas as pd
 
 st.title("Production Possibility Curves")
 st.sidebar.header("Settings")
@@ -10,9 +11,14 @@ e_y = st.sidebar.slider("efficiency of oranges", 1, 10, 5, step=1)
 
 frogs = np.arange(0,40,0.1)
 oranges = e_y*np.sqrt(L-(frogs/e_x)**2)
-
+chart_data = pd.DataFrame(
+    {
+        "col1": frogs,
+        "col2": oranges,
+    }
+)
 # fig = plotly.line(x=frogs,y=oranges)
 # fig.layout.xaxis.fixedrange = True
 # fig.layout.yaxis.fixedrange = True
-st.line_chart([frogs,oranges], x="frogs", y="oranges")
+st.line_chart(chart_data, x="frogs", y="oranges")
 
