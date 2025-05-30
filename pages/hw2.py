6d7c738d24212799a97b16a58e4fdee8b0634735
Baipalm <ttamajai@ucsb.edu>
2025-05-29 20:09:06 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -14,8 +14,8 @@ y = e_y*np.sqrt(L-(x/e_x)**2)
 fig = plotly.line(x=x,y=y)
 fig.update_layout(
        xaxis_range=[0, 30],  # Set x-axis range from 0 to 4
-       yaxis_range=[0, 30]   # Set y-axis range from 0 to 7
-       width=100  # Set a fixed width in pixels
+       yaxis_range=[0, 30],   # Set y-axis range from 0 to 7
+       width=100,  # Set a fixed width in pixels
        height=100  # Set a fixed height in pixels
    )
 st.plotly_chart(fig)
