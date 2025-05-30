4e1d44479353ea7a4f05088893d2cc6e293d23a4
Baipalm <ttamajai@ucsb.edu>
2025-05-29 19:52:51 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -5,8 +5,8 @@ import numpy as np
 st.title("Production Possibility Curves")
 st.sidebar.header("NMF Convergence Settings")
 L = st.sidebar.slider("Total Labour", 10, 100, 50, step=10)
-e_1 = st.sidebar.slider("efficiency of oranges", 1, 20, 6, step=1)
-e_2 = st.sidebar.slider("efficiency of frogs", 1, 20, 10, step=1)
+e_x = st.sidebar.slider("efficiency of oranges", 1, 20, 6, step=1)
+e_y = st.sidebar.slider("efficiency of frogs", 1, 20, 10, step=1)
 
 x = np.arange(0,10,0.1)
 y = e_y*np.sqrt(L-(x/e_x)**2)
