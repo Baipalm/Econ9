03766f517837a2acffed5bd44b78ed8f5b502590
Baipalm <ttamajai@ucsb.edu>
2025-05-29 20:34:47 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -9,7 +9,7 @@ L = st.sidebar.slider("Total Labour", 10, 100, 50, step=10)
 e_x = st.sidebar.slider("efficiency of frogs", 1, 10, 5, step=1)
 e_y = st.sidebar.slider("efficiency of oranges", 1, 10, 5, step=1)
 
-frogs = np.arange(0,40,0.05)
+frogs = np.arange(0,50,0.1)
 oranges = e_y*np.sqrt(L-(frogs/e_x)**2)
 data = np.hstack((frogs,oranges))
 # fig = plotly.line(x=frogs,y=oranges)
