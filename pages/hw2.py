f3fb5eff92e1357b9d7f2c09efc9d110456d5dd8
Baipalm <ttamajai@ucsb.edu>
2025-05-29 21:34:25 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -9,17 +9,19 @@ L = st.sidebar.slider("Labour", min_value=1, max_value=40, value=15, step=1)
 st.sidebar.subheader("Production Efficiency")
 e_x = st.sidebar.slider("🐸", 1, 20, 5, step=1)
 e_y = st.sidebar.slider("🟠", 1, 20, 5, step=1)
-
-frogs = np.arange(0,200,0.05)
+max_frogs = e_x * np.sqrt(L)
+frogs = np.linspace(0, max_frogs, 500)
 oranges = e_y*np.sqrt(L-(frogs/e_x)**2)
 data = np.hstack((frogs,oranges))
 df = pd.DataFrame({
         'x': frogs,
         'y': oranges
     })
+max_x = e_x * np.sqrt(L)
+max_y = e_y * np.sqrt(L)
 chart = alt.Chart(df).mark_line().encode(
-    alt.X('x:Q', scale=alt.Scale(domain=[0, np.sqrt(40)*20*1.1])),
-    alt.Y('y:Q', scale=alt.Scale(domain=[0, np.sqrt(40)*20*1.1])),
+    alt.X('x:Q', scale=alt.Scale(domain=[0, max_x*1.1])),
+    alt.Y('y:Q', scale=alt.Scale(domain=[0, max_y*1.1])),
 )
 st.altair_chart(chart.properties(width=600, height=600)) 
 
