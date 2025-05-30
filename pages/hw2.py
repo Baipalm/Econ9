ade78a81c77ef5af25205bdc59a540c4b6970950
Baipalm <ttamajai@ucsb.edu>
2025-05-29 19:53:51 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -3,7 +3,7 @@ import plotly.express as plotly
 import numpy as np
 
 st.title("Production Possibility Curves")
-st.sidebar.header("NMF Convergence Settings")
+st.sidebar.header("Settings")
 L = st.sidebar.slider("Total Labour", 10, 100, 50, step=10)
 e_x = st.sidebar.slider("efficiency of oranges", 1, 20, 6, step=1)
 e_y = st.sidebar.slider("efficiency of frogs", 1, 20, 10, step=1)
