3febf8f4dbc1328fb376dbdbd3cc60dff053e840
Baipalm <ttamajai@ucsb.edu>
2025-05-29 20:25:10 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -11,14 +11,9 @@ e_y = st.sidebar.slider("efficiency of oranges", 1, 10, 5, step=1)
 
 frogs = np.arange(0,40,0.1)
 oranges = e_y*np.sqrt(L-(frogs/e_x)**2)
-chart_data = pd.DataFrame(
-    {
-        "col1": frogs,
-        "col2": oranges,
-    }
-)
+
 # fig = plotly.line(x=frogs,y=oranges)
 # fig.layout.xaxis.fixedrange = True
 # fig.layout.yaxis.fixedrange = True
-st.line_chart(chart_data, x="frogs", y="oranges")
+st.line_chart(x = frogs, y = oranges, xlabel="frog", ylabel="orange")
 
