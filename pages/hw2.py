3f97a74e27248ec9b3ff2568aa55caea9ba1c4e6
Baipalm <ttamajai@ucsb.edu>
2025-05-29 20:12:26 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -9,7 +9,7 @@ e_x = st.sidebar.slider("efficiency of frogs", 1, 10, 5, step=1)
 e_y = st.sidebar.slider("efficiency of oranges", 1, 10, 5, step=1)
 
 frogs = np.arange(0,200,1)
-oranges = e_y*np.sqrt(L-(x/e_x)**2)
+oranges = e_y*np.sqrt(L-(frogs/e_x)**2)
 
 fig = plotly.line(x=x,y=y)
 fig.layout.xaxis.fixedrange = True
