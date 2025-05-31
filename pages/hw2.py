14bd8b906f4c0027c26830838f226c6e22920537
Baipalm <ttamajai@ucsb.edu>
2025-05-31 12:22:50 -0700

    Update hw2.py

--- pages/hw2.py
+++ pages/hw2.py
@@ -9,9 +9,7 @@ import pandas as pd
 
 def frogs_to_oranges(frogs,e_x,e_y,L):
         oranges_squared = e_y**2 * ( L - (frogs / e_x)**2 )
-        for i in oranges_squared:
-                if oranges_squared[i] <= 0:
-                        oranges_squared[i]=0
+        oranges_squared[ oranges_squared <= 0 ] = 0
         oranges = np.sqrt(oranges_squared)
         return oranges
         
