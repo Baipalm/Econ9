b6ea6c3609252339390a7baf3fbd2281baebb549
Baipalm <ttamajai@ucsb.edu>
2025-06-02 21:10:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ with st.expander(""):
      """)
 
 st.markdown("#### References")
-st.markdown("""
+st.multiline("""
 [1] Santa Ana College. “Inquiry-Based Learning.” Sac.edu, 2024, sac.edu/AcademicAffairs/TracDat/Pages/Inquiry-Based-Learning-.aspx.
 [2] de Jong, Ton, et al. “Let’s Talk Evidence – the Case for Combining Inquiry-Based and Direct Instruction.” Educational Research Review, 
 vol. 39, no. 39, May 2023, p. 100536, www.sciencedirect.com/science/article/pii/S1747938X23000295, https://doi.org/10.1016/j.edurev.2023.100536.
