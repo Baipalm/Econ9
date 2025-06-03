b9a5856a220120ad6eb3097a6d44710c9b0973a3
Baipalm <ttamajai@ucsb.edu>
2025-06-02 19:07:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ st.title('How can you apply inquiry based learning to Economics')
 st.write("**Question:** What is Inquiry Based Learning?")
 
 with st.expander(""):
-    st.write("According to Santa Ana College, Inquiry-based learning reframes learning as a student-centered activity. Particularly it is defined by and
+    st.write("""
+    According to Santa Ana College, Inquiry-based learning reframes learning as a student-centered activity. Particularly it is defined by and
     experimental and problem-centric approach. As opposed to traditional lectures where student are passively listening to instructors, inquiry based learning
-    wants students to be active and participate through guided questions.")
+    wants students to be active and participate through guided questions.""")
