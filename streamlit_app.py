245717267df5c66d3a8b14f255aa5c4621c7e30d
Baipalm <ttamajai@ucsb.edu>
2025-06-02 21:08:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,33 @@
 import streamlit as st
 
 st.title('How can you apply inquiry based learning to Economics')
-st.write("**Question:** What is Inquiry Based Learning?")
+st.write("** What is Inquiry Based Learning?**")
 
 with st.expander(""):
     st.write("""
     According to Santa Ana College, Inquiry-based learning reframes learning as a student-centered activity. Particularly it is defined by and
-    experimental and problem-centric approach. As opposed to traditional lectures where student are passively listening to instructors, inquiry based learning
+    experimental and problem-centric approach^[1]. As opposed to traditional lectures where student are passively listening to instructors, inquiry based learning
     wants students to be active and participate through guided questions.""")
+
+# insert diagram of inquirybased learning
+
+st.write("** Why Inquiry Based Learning and Economics?**")
+
+with st.expander(""):
+    st.write(""" In inquiry based learning empowers people to take control of their learning. They are guided to set their own goals and really learn what they want! One of the fundamental
+    tenants of economics is the construction of models, these tiny conceptions allow us to make powerful generalizations and are perfect to explore! The graphs of economics are simplistic and 
+    power whose nuance can only be developed by playing around with graphs!
+     """)
+# insert nice image maybe graph
+st.write("**Is Inquiry based learning better than direct instruction**")
+
+with st.expander(""):
+    st.write(""" No, not necessarily according to science direct there are proponents to both sides and many even prefer direction!^[2]
+     """)
+
+st.markdown("#### References")
+st.markdown("""
+[1] Santa Ana College. “Inquiry-Based Learning.” Sac.edu, 2024, sac.edu/AcademicAffairs/TracDat/Pages/Inquiry-Based-Learning-.aspx.
+[2] de Jong, Ton, et al. “Let’s Talk Evidence – the Case for Combining Inquiry-Based and Direct Instruction.” Educational Research Review, 
+vol. 39, no. 39, May 2023, p. 100536, www.sciencedirect.com/science/article/pii/S1747938X23000295, https://doi.org/10.1016/j.edurev.2023.100536.
+""")
